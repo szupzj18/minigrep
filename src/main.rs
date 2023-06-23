c9187df3586e3dd707fe3a70b9e3bcc694cd857f
szupzj18 <szupzj18@outlook.com>
2023-06-23 11:54:29 +0800

    feat(minigrep): using stderr output error.

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use minigrep::{Config};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else( |err| {
-        println!("Problem while parsing config: {err}");
+        eprintln!("Problem while parsing config: {err}"); // use {eprintlin!} error goes to io::stderr
         process::exit(1); // process error exit.
     });
     
     if let Err(e) = minigrep::run(config) {
-        println!("Application Error! {e}");
+        eprintln!("Application Error! {e}");
         process::exit(1);
     }
 }
